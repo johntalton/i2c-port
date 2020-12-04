f1747073888ba94d54f6c047651090ddc9ffc470
johntalton <johntalton@gmail.com>
2020-12-04 02:18:53 -0500

    frontend testing and doc

--- README.md
+++ README.md
@@ -1,7 +1,33 @@
 # I²C MessagePort based Bus
 
-This library and example intends to provide a `I2CBus` implementation (using async/await) over a `MessagePort`.
+Provides tooling that allows for `I2CBus` hosting natively, over `MessagePort`, within `Worker` and over `WebSockets`.
 
-Primary target use cases:
- - serving multiple `Worker` threads with single bus worker
- - proxy `MessagePort` over HTTP (WebSockets)
+This allows the `I2CBus` api to be used in a wide range of deployment cases.
+
+It can also be using with bus Multiplexing such as [@johntalton/i2c-bus-tca9548a](https://github.com/johntalton/i2c-bus-tca9548a) that adheres to the `I2CBus` interface.  As well as using [i2c-bus](https://github.com/fivdi/i2c-bus) as the default concrete/base implementation.
+
+### Provided Abstraction
+Key is providing multiple abstraction layers.
+
+- A `Message` type definition (including `Read`, `ReadResult`, `Error`, etc)
+- An `I2CPort` function that maps `Message` into `I2CBus` commands on provided `bus`
+- And `I2CPortBus` which implements `I2CBus` over a `MessagePort` interface
+
+The corresponding `WebSocket` to `MessagePort` example can be run to provide this service.
+
+The **Message** definition layer provides a naming convention and contract without implementation details. It is ideal for abstracting the service at each layer.
+
+The **Port** utility layer allows for mapping of the generalized message into `I2CBus` interface commands. This layer provides the mirror binding the `I2CPortBus` implementation.
+This utility operates over an `I2CBus` interface, allowing for further abstraction and requires the caller to manager its allocation and state.
+
+The **Bus** interface layer (along with example server) creates a `WebSocket` service which allows for the above `Message` interactions to a concrete - and remote - implementation.
+The `I2CPortBus` uses a simple one-call-one-response per `MessagePort` in order to bridge the `WebSocket` `on`/`send` api into the `I2CBus` more friendly `Promise`based async/await code.
+
+### Direction
+
+Having a full `I2CWebBus` that implements `I2CBus` enables moving other Sensor implementations that currently use the `I2CBus` over to a web-safe version. This will increase code portability and quality as wall as validate many of the timing and latency issues that are otherwise not seen locally in most cases.
+
+Further mocking and virtualizing can be provided once a common `WebSocket` message api is established. Providing virtual sensor and other services in a web-centric fashion.
+
+Single 1:1 calls allow for basic interactions, however, extending the message with 'scripting' style operation could provide performant solution when latency of the transit layer becomes and issue.
+This could be extended to enable service-side interactions such as keep alive or default access polling. Other common data-fetch or read-comp-trigger features could be added.
--- example/web.js
+++ example/web.js
@@ -7,48 +7,50 @@ const WebSocket = require('ws')
 //const { v4: uuidv4 } = require('uuid');
 const morgan = require('morgan')
 
-const i2cWorker = new Worker(__dirname + '/service.js')
-i2cWorker.onmessage = event => console.log('worker said', event)
-
 const app = express()
 const MORGAN_EXT = ':status :method :url HTTP/:http-version  :remote-addr @ :response-time ms\x1b[0m'
 app.use(morgan(MORGAN_EXT))
 app.use('/', express.static('example/web'))
 app.use((req, res, next) => next(new Error('🎁 ' + req.originalUrl)))
 
-const wsServer = new WebSocket.Server({ noServer: true });
-
-wsServer.on('connection', async function connection(ws, req) {
-
-  const mc = new MessageChannel()
-  i2cWorker.postMessage({ bus: 1, port: mc.port2 }, [ mc.port2 ])
-
-  ws.on('message', async function incoming(message) {
-    console.log('received', message);
-    const msg = JSON.parse(message)
-    if("type" in msg) { mc.port1.postMessage(msg) }
-  });
-  ws.on('error', e => console.log('error', e))
-  ws.on('close', () => console.log('close'))
-
-  mc.port1.on('message', message => {
-    console.log('result', message)
-    ws.send(JSON.stringify(message))
-  })
-});
-
 const server = app.listen(9000, () => console.log('Service Up'))
 
-server.on('upgrade', (request, socket, head) => {
-  const protocols = request.headers['sec-websocket-protocol']
-    ?.split(',')?.map(s => s.trim())
-    ?? []
+if(false) {
+  const i2cWorker = new Worker(__dirname + '/service.js')
+  i2cWorker.onmessage = event => console.log('worker said', event)
 
-  const pathname = url.parse(request.url).pathname;
-  console.log('path / protocols', pathname, protocols)
-  if(!protocols.includes('i2c')) { console.log('no matching protocol - drop'); socket.destroy() }
+  const wsServer = new WebSocket.Server({ noServer: true });
 
-  wsServer.handleUpgrade(request, socket, head, socket => {
-    wsServer.emit('connection', socket, request);
+  wsServer.on('connection', async function connection(ws, req) {
+
+    const mc = new MessageChannel()
+    i2cWorker.postMessage({ bus: 1, port: mc.port2 }, [ mc.port2 ])
+
+    ws.on('message', async function incoming(message) {
+      console.log('received', message);
+      const msg = JSON.parse(message)
+      if("type" in msg) { mc.port1.postMessage(msg) }
+    });
+    ws.on('error', e => console.log('error', e))
+    ws.on('close', () => console.log('close'))
+
+    mc.port1.on('message', message => {
+      console.log('result', message)
+      ws.send(JSON.stringify(message))
+    })
   });
-});
+
+  server.on('upgrade', (request, socket, head) => {
+    const protocols = request.headers['sec-websocket-protocol']
+      ?.split(',')?.map(s => s.trim())
+      ?? []
+
+    const pathname = url.parse(request.url).pathname;
+    console.log('path / protocols', pathname, protocols)
+    if(!protocols.includes('i2c')) { console.log('no matching protocol - drop'); socket.destroy() }
+
+    wsServer.handleUpgrade(request, socket, head, socket => {
+      wsServer.emit('connection', socket, request);
+    });
+  })
+}
