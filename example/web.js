6b1056f249de257e3594fdff465a248b1103cb9f
johntalton <johntalton@gmail.com>
2020-12-08 13:30:18 -0500

    refactor web for decoupling

--- example/web.js
+++ example/web.js
@@ -7,7 +7,6 @@ const WebSocket = require('ws')
 //const { v4: uuidv4 } = require('uuid');
 const morgan = require('morgan')
 
-
 const hostOnly = process.argv.includes('--hostOnly')
 const MORGAN_EXT = ':status :method :url HTTP/:http-version  :remote-addr @ :response-time ms\x1b[0m'
 
@@ -20,61 +19,89 @@ const app = express()
 const server = app.listen(9000, () => console.log('Service Up'))
 
 if(!hostOnly) {
+  function handleWSConnectionOverServerPort(servicePort, serviceName) {
+    function portMessageToStringMessage(message) {
+      const msg = message
+      if(msg.buffer !== undefined) {
+        msg.buffer = Buffer.from(message.buffer).toString('base64')
+      }
 
-  function postMessageOnPort(port, msg) {
-    if(msg.type !== undefined) {
-      if(msg.buffer) {
-        port.postMessage(msg, [ msg.buffer.buffer ])
+      return JSON.stringify(msg)
+    }
+
+    function stringMessageToPortMessage(message) {
+        const encodedMsg = JSON.parse(message)
+
+        const msg = encodedMsg
+        if(msg.buffer) {
+          const buf = Buffer.from(msg.buffer, 'base64')
+            .toString()
+            .split()
+            .map(b => b.charCodeAt(0))
+
+          //console.log('decoding', buf)
+          msg.buffer = Buffer.from(buf)
+        }
+
+        return msg
+    }
+
+    function postMessageOnPort(port, msg) {
+      if(msg.type !== undefined) {
+        if(msg.buffer) {
+          port.postMessage(msg, [ msg.buffer.buffer ])
+        }
+        else {
+          port.postMessage(msg)
+        }
       }
-      else {
-        port.postMessage(msg)
+    }
+
+    function handleWSMessageOverPort(port) {
+      return message => postMessageOnPort(port, stringMessageToPortMessage(message))
+    }
+
+    function handleWSError(e) {
+      console.log('ws error ', serviceName, e)
+    }
+
+    function handleWSCloseOverMessagePort(port) {
+      return () => {
+        console.log('ws close - close port to service', serviceName)
+        port.close()
       }
+    }
+
+    function handlePortMessageOverWS(ws) {
+      return message => ws.send(portMessageToStringMessage(message))
+    }
+
+    function handlePortCloseOverWS(ws) {
+      return event => {
+        console.log('client channel close', event)
+        ws.close()
+      }
+    }
+    // handleWSConnection
+    return (ws, req) => {
+      // inform the service of our intention to communicate
+      const mc = new MessageChannel()
+      servicePort.postMessage({ port: mc.port2 }, [ mc.port2 ])
+
+      // setup WS handlers
+      // these interact with the client webSocket using the creation
+      // of a side-channel `mc` and acting as a proxy between
+      ws.on('message', handleWSMessageOverPort(mc.port1))
+      ws.on('error', handleWSError)
+      ws.on('close', handleWSCloseOverMessagePort(mc.port1))
+
+      // bind service handlers
+      mc.port1.on('message', handlePortMessageOverWS)
+      mc.port1.on('close', handlePortCloseOverWS)
     }
   }
 
-  function createI2cWorker(serviceUrl) {
-    const i2cWorker = new Worker(serviceUrl)
-    i2cWorker.on('message', event => console.log('worker said', event))
-    i2cWorker.on('exit', event => {
-      console.log('worker exit', event)
-      // TODO server.close()
-    })
-  }
-
-  const serviceUrl = __dirname + 'service.js' // user path.concat
-  const i2cWorker = createI2cWorker(serviceUrl)
-
-  const wsServer = new WebSocket.Server({ noServer: true });
-
-  wsServer.on('connection', async function connection(ws, req) {
-
-    const mc = new MessageChannel()
-    i2cWorker.postMessage({ port: mc.port2 }, [ mc.port2 ])
-
-    ws.on('message', async message => {
-      console.log('ws to i2c: ', message);
-      const msg = stringMessageToPortMessage(message)
-      postMessageOnPort(mc.port1, msg)
-    })
-
-    ws.on('error', e => console.log('ws error', e))
-
-    ws.on('close', () => {
-      console.log('ws close - close port to i2c')
-      mc.port1.close()
-    })
-
-    mc.port1.on('message', message => {
-      const msg = portMessageToStringMessage(message)
-      ws.send(msg)
-    })
-    mc.port1.on('close', event => {
-      console.log('client channel close')
-      ws.close()
-    })
-  });
-
-  server.on('upgrade', (request, socket, head) => {
+  function handleWSUpgrade(request, socket, head) {
     //const ip = req.headers['x-forwarded-for'].split(/\s*,\s*/)[0]
     //const ip = req.socket.remoteAddress
     const pathname = url.parse(request.url).pathname
@@ -93,5 +120,14 @@ if(!hostOnly) {
     wsServer.handleUpgrade(request, socket, head, socket => {
       wsServer.emit('connection', socket, request)
     });
-  })
+  }
+
+  const serviceUrl = __dirname + 'service.js' // user path.concat
+  const i2cWorker = new Worker(serviceUrl)
+  i2cWorker.on('message', event => console.log('worker said', event))
+  i2cWorker.on('exit', event => console.log('worker exit', event))
+
+  const wsServer = new WebSocket.Server({ noServer: true })
+  wsServer.on('connection', handleWSConnectionOverServerPort(wsServer, 'i2c'));
+  server.on('upgrade', handleWSUpgrade)
 }
