b3abccb8de718453d81eeb42dcc38a41e2ff7fde
johntalton <johntalton@gmail.com>
2020-12-10 15:24:20 -0500

    refactor messages - eslint - read write impl

--- src/bus.ts
+++ src/bus.ts
@@ -5,11 +5,12 @@ import {
   I2CReadResult, I2CWriteResult
 } from '@johntalton/and-other-delights'
 
-import { ReadWrite  } from './messages' 
+import { ReadWrite  } from './messages'
 
 export class I2CPortBus implements I2CBus {
   readonly port: MessagePort
   readonly busNumber: I2CBusNumber
+  readonly namespace: string
 
   static openPromisified(providerPort: MessagePort, busNumber: I2CBusNumber): Promise<I2CPortBus> {
     return Promise.resolve(new I2CPortBus(providerPort, busNumber))
@@ -18,9 +19,10 @@ export class I2CPortBus implements I2CBus {
   private constructor(port: MessagePort, busNumber: I2CBusNumber) {
     this.port = port;
     this.busNumber = busNumber
+    this.namespace = ''
   }
 
-  close(): void {}
+  close(): void { this.port.close() }
 
   private static fire<R>(port: MessagePort, call: ReadWrite): Promise<R> {
     return new Promise((resolve, reject) => {
@@ -47,7 +49,7 @@ export class I2CPortBus implements I2CBus {
     })
   }
 
-  private static async sideChannelFire<R>(port: MessagePort,call: ReadWrite): Promise<R> {
+  private static async sideChannelFire<R>(port: MessagePort, call: ReadWrite): Promise<R> {
     const mc = new MessageChannel()
     port.postMessage({ port: mc.port2 }, [ mc.port2 ])
     return I2CPortBus.fire(mc.port1, call)
@@ -55,22 +57,54 @@ export class I2CPortBus implements I2CBus {
 
 
   sendByte(address: I2CAddress, byte: number): Promise<void> {
-    return I2CPortBus.sideChannelFire(this.port, { bus: this.busNumber, type: 'sendByte', address, byte})
+    return I2CPortBus.sideChannelFire(this.port, {
+      namespace: this.namespace,
+      type: 'sendByte',
+      bus: this.busNumber,
+      address,
+      byte
+    })
   }
 
   readI2cBlock(address: I2CAddress, cmd: number, length: number, buffer: Buffer): Promise<I2CReadResult> {
-    return I2CPortBus.sideChannelFire(this.port, { bus: this.busNumber, type: 'readBlock', address, cmd, length })
+    return I2CPortBus.sideChannelFire(this.port, {
+      namespace: this.namespace,
+      type: 'readBlock',
+      bus: this.busNumber,
+      address,
+      cmd,
+      length
+    })
   }
 
   writeI2cBlock(address: I2CAddress, cmd: number, length: number, buffer: Buffer): Promise<I2CWriteResult> {
-    return I2CPortBus.sideChannelFire(this.port, { bus: this.busNumber, type: 'writeBlock', address, cmd, buffer })
+    return I2CPortBus.sideChannelFire(this.port, {
+      namespace: this.namespace,
+      type: 'writeBlock',
+      bus: this.busNumber,
+      address,
+      cmd,
+      buffer: buffer.slice(0, length)
+    })
   }
 
   i2cRead(address: I2CAddress, length: number, buffer: Buffer): Promise<I2CReadResult> {
-    return I2CPortBus.sideChannelFire(this.port, { bus: this.busNumber, type: 'read', address, length })
+    return I2CPortBus.sideChannelFire(this.port, {
+      namespace: this.namespace,
+      type: 'read',
+      bus: this.busNumber,
+      address,
+      length
+    })
   }
 
   i2cWrite(address: I2CAddress, length: number, buffer: Buffer): Promise<I2CWriteResult> {
-    return I2CPortBus.sideChannelFire(this.port, { bus: this.busNumber, type: 'write', address, buffer })
+    return I2CPortBus.sideChannelFire(this.port, {
+      namespace: this.namespace,
+      type: 'write',
+      bus: this.busNumber,
+      address,
+      buffer: buffer.slice(0, length)
+    })
   }
 }
--- src/messages.ts
+++ src/messages.ts
@@ -1,16 +1,21 @@
-export type Message = { type: string, opaque?: string }
+export type WithNamespace = { namespace: string | '' }
+export type WithType = { type: string }
+export type WithOpaque = { opaque?: string }
+export type WithAddress = { bus: number, address: number }
 
-export type Addressed = { bus: number, address: number } & Message
-export type Command = { cmd: number } & Addressed
-//export type WithBuffer = { buffer: ArrayBuffer }
-export type WithBuffer = { buffer: Buffer }
+export type WithCommand = { cmd: number } & WithAddress
+export type WithNodeBuffer = { buffer: Buffer | Uint8Array | Uint8ClampedArray }
+export type WithArrayBuffer = { buffer: ArrayBuffer }
+export type WithBuffer = WithNodeBuffer
 
-export type SendByte = { type: 'sendByte', byte: number } & Addressed
-export type Read = { type: 'read', length: number } & Addressed
-export type Write = { type: 'write' } & WithBuffer & Addressed
+export type Message = WithNamespace & WithOpaque & WithType & WithAddress
 
-export type ReadBlock = { type: 'readBlock', length: number } & Command
-export type WriteBlock = { type: 'writeBlock' } & WithBuffer & Command
+export type SendByte = { type: 'sendByte', byte: number } & Message
+export type Read = { type: 'read', length: number } & Message
+export type Write = { type: 'write' } & WithBuffer & Message
+
+export type ReadBlock = { type: 'readBlock', length: number } & WithCommand & Message // WithReadBuffer
+export type WriteBlock = { type: 'writeBlock' } & WithBuffer & WithCommand & Message
 
 export type ReadResult = { type: 'readResult', bytesRead: number } & WithBuffer & Message | Error
 export type WriteResult = { type: 'writeResult', bytesWritten: number } & Message | Error
@@ -20,5 +25,3 @@ export type Success = { type: 'success' } & Message
 
 export type ReadWrite = SendByte | Read | Write | ReadBlock | WriteBlock
 export type Result = ReadResult | WriteResult | Success | Error
-
-
