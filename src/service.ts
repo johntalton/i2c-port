9d8b87215a395375a7103e3b91a8a5fc605cbb52
johntalton <johntalton@gmail.com>
2021-03-16 22:54:57 -0400

    export service

--- src/service.ts
+++ src/service.ts
@@ -1,31 +1,33 @@
+import { I2CBus } from '@johntalton/and-other-delights'
 import { I2CPort } from './port.js'
 
-export function i2cMultiPortService(servicePort, bus) {
+export class I2CPortService {
+  static from(servicePort: MessagePort, bus: I2CBus): void {
+    const clients = new Set()
 
-  const clients = new Set()
+    servicePort.on('message', connectMessage => {
+      console.log('service port client connect message')
+      const { port } = connectMessage
 
-  servicePort.on('message', connectMessage => {
-    console.log('service port client connect message')
-    const { port } = connectMessage
+      clients.add(port)
 
-    clients.add(port)
+      port.on('message', async clientMessage => {
+        const result = await I2CPort.handleMessage(bus, clientMessage)
+        port.postMessage(result, result.buffer !== undefined ? [ result.buffer ] : [])
+      })
 
-    port.on('message', async clientMessage => {
-      const result = await I2CPort.handleMessage(bus, clientMessage)
-      port.postMessage(result, result.buffer !== undefined ? [ result.buffer ] : [])
+      port.on('close', () => {
+        console.log('client port goodbye')
+
+        if(!clients.has(port)) { console.warn('client port not in clients list') }
+        clients.delete(port)
+        port.close()
+      })
+
+      port.on('messageerror', e => console.warn('client port message error', e))
     })
 
-    port.on('close', () => {
-      console.log('client port goodbye')
-
-      if(!clients.has(port)) { console.warn('client port not in clients list') }
-      clients.delete(port)
-      port.close()
-    })
-
-    port.on('messageerror', e => console.warn('client port message error', e))
-  })
-
-  servicePort.on('messageerror', error => console.warn('service port message error', error))
-  servicePort.on('close', () => { console.log('close service port'); clients.forEach(p => p.close()) })
+    servicePort.on('messageerror', error => console.warn('service port message error', error))
+    servicePort.on('close', () => { console.log('close service port'); clients.forEach(p => p.close()) })
+  }
 }
