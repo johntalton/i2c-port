c65e9b82276c8a1333edbb8329499188b0f1ab13
johntalton <johntalton@gmail.com>
2021-03-19 13:38:33 -0400

    clean and dom

--- src/service.ts
+++ src/service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-undefined */
 import { I2CBus } from '@johntalton/and-other-delights'
 import { I2CPort } from './port.js'
 
@@ -6,7 +7,6 @@ export class I2CPortService {
     const clients = new Set()
 
     servicePort.on('message', connectMessage => {
-      console.log('service port client connect message')
       const { port } = connectMessage
 
       clients.add(port)
@@ -17,17 +17,13 @@ export class I2CPortService {
       })
 
       port.on('close', () => {
-        console.log('client port goodbye')
+        if(!clients.has(port)) { return }
 
-        if(!clients.has(port)) { console.warn('client port not in clients list') }
         clients.delete(port)
         port.close()
       })
-
-      port.on('messageerror', e => console.warn('client port message error', e))
     })
 
-    servicePort.on('messageerror', error => console.warn('service port message error', error))
-    servicePort.on('close', () => { console.log('close service port'); clients.forEach(p => p.close()) })
+    servicePort.on('close', () => clients.forEach(p => p.close()))
   }
 }
